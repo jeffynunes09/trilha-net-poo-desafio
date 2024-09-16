2d03c06da3994d60560a2849569f4db409eb8629
Jefferson Nunes <nunesjefferson425@gmail.com>
2024-09-16 20:20:16 -0300

    finalizando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Iphone cel1 = new Iphone("12345678","IPHONE 7","12345678", 4 );
+
+
+
+cel1.Ligar();
+cel1.InstalarAplicativo("WhatsApp");
